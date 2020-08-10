28f580b0c93720472bd18afafd182f782f24b28e
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-10 14:24:27 -0700

    add the Edit and addRecipe features to the ingredientsController
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- RecipeBox/Controllers/IngredientsController.cs
+++ RecipeBox/Controllers/IngredientsController.cs
@@ -54,6 +54,51 @@ namespace RecipeBox.Controllers
       return RedirectToAction("Details", new { id = ingredient.IngredientId });
     }
 
-    
+    public ActionResult Delete(int id)
+    {
+      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId ==id);
+      return View(thisIngredient);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
+      _db.Ingredients.Remove(thisIngredient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public ActionResult DeleteRecipe(int joinId)
+    {
+      var joinEntry = _db.RecipeIngredient.FirstOrDefault(entry => entry.RecipeIngredientId == joinId);
+      _db.RecipeIngredient.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddRecipe(int id)
+    {
+      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
+      ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
+      return View(thisIngredient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Ingredient ingredient, int RecipeId)
+    {
+      var testVariable = _db.RecipeIngredient.FirstOrDefault(join => join.IngredientId == ingredient.IngredientId && join.RecipeId == RecipeId);
+
+      if (testVariable != null)
+      {
+        _db.Entry(ingredient).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Details", new { id = ingredient.IngredientId });
+      }
+      if (RecipeId != 0)
+      {
+        _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
+      }
+      _db.Entry(ingredient).State = EntityState.Modified;
+    }
   }
 }
