4c39a15a753d852db74635b395d8ba7c9495b4fb
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-10 11:06:27 -0700

    add our join models and code
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

---
+++ RecipeBox/Models/RecipeIngredient.cs
@@ -0,0 +1,11 @@
+namespace RecipeBox.Model
+{
+  public class RecipeIngredient
+  {
+    public int RecipeIngredientId { get; set; }
+    public int RecipeId { get; set; }
+    public int IngredientId {get; set; }
+    public Recipe Recipe { get; set; }
+    public Ingredient Ingredient { get; set; }
+  }
+}
