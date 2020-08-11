68618b0ef5ea943894f57ecf116447ec8ee0d08c
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-11 14:31:26 -0700

    add the edit and delete routes to the RecipesController
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -40,10 +40,41 @@ namespace RecipeBox.Controllers
       return RedirectToAction("Index");
     }
 
-    public ActionResult Details(int id)
+    // public ActionResult Details(int id)
+    // {
+    //   var thisRecipe = _db.Recipes;
+    //   return View(thisRecipe);
+    // }
+    public ActionResult Edit(int id)
     {
-      var thisRecipe = _db.Recipes;
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
       return View(thisRecipe);
     }
+
+    [HttpPost]
+    public ActionResult Edit(Recipe recipe)
+    {
+      _db.Entry(recipe).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      return View(thisRecipe);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      _db.Recipes.Remove(thisRecipe);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
+
   }
 }
