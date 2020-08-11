f8089440f2753c0511826d86308df8320420c404
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-11 13:53:49 -0700

    add the beginning to RecipesController, including the index route
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

---
+++ RecipeBox/Controllers/RecipesController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RecipeBox.Controllers
+{
+  public class RecipesController : Controller
+  {
+    private readonly RecipeBoxContext _db;
+
+    public RecipesController(RecipeBoxContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Recipe> model = _db.Recipes.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Name");
+      return View();
+    }
+  }
+}
