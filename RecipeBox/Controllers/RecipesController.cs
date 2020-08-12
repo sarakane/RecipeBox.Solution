2be4d022ba190ea8b16d03098b2a659414cbb3cb
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-12 11:51:51 -0700

    add the recipe details page and confirm that it shows both associated tags and ingredients properly
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -40,11 +40,16 @@ namespace RecipeBox.Controllers
       return RedirectToAction("Index");
     }
 
-    // public ActionResult Details(int id)
-    // {
-    //   var thisRecipe = _db.Recipes;
-    //   return View(thisRecipe);
-    // }
+    public ActionResult Details(int id)
+    {
+      var thisRecipe = _db.Recipes
+        .Include(recipe => recipe.Tags)
+        .ThenInclude(join => join.Tag)
+        .Include(recipe => recipe.Ingredients)
+        .ThenInclude(join => join.Ingredient)
+        .FirstOrDefault(recipe => recipe.RecipeId == id);
+      return View(thisRecipe);
+    }
     public ActionResult Edit(int id)
     {
       var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
@@ -81,11 +86,51 @@ namespace RecipeBox.Controllers
       return View(thisRecipe);
     }
 
+    [HttpPost]
+    public ActionResult AddTag(Recipe recipe, int TagId)
+    {
+      if (TagId != 0 && !_db.TagRecipe.Any(x => x.RecipeId == recipe.RecipeId && x.TagId == TagId))
+      {
+        _db.TagRecipe.Add(new TagRecipe() { TagId = TagId, RecipeId = recipe.RecipeId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = recipe.RecipeId });
+    }
+
     public ActionResult AddIngredient(int id)
     {
       var thisRecipe = _db.Recipes.FirstOrDefault(recipes => recipes.RecipeId ==id);
       ViewBag.IngredientId = new SelectList(_db.Ingredients, "IngredientId", "Name");
       return View(thisRecipe);
+    }
+
+    [HttpPost]
+    public ActionResult AddIngredient(Recipe recipe, int IngredientId)
+    {
+      if(IngredientId != 0 && _db.RecipeIngredient.Any(x => x.RecipeId == recipe.RecipeId && x.IngredientId == IngredientId))
+      {
+        _db.RecipeIngredient.Add(new RecipeIngredient() { IngredientId = IngredientId, RecipeId = recipe.RecipeId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = recipe.RecipeId });
+    }
+
+    [HttpPost]
+    public ActionResult RemoveTag(int joinId)
+    {
+      var joinEntry = _db.TagRecipe.FirstOrDefault(entry => entry.TagRecipeId == joinId);
+      _db.TagRecipe.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.RecipeId});
+    }
+
+    [HttpPost]
+    public ActionResult RemoveIngredient(int joinId)
+    {
+      var joinEntry = _db.RecipeIngredient.FirstOrDefault(entry => entry.RecipeIngredientId == joinId);
+      _db.RecipeIngredient.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.RecipeId });
     }
   }
 }
